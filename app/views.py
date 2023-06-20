388f0f55ef673764b2a098f9723251448169b251
Vladimir <vovcic.copcic@gmail.com>
2023-06-20 07:55:37 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,67 @@
+﻿"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .forms import RegistrationForm
+from django.urls import reverse
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
+from django.urls import reverse
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirecționează utilizatorul către pagina de autentificare
+            return redirect(reverse('login'))
+    else:
+        form = RegistrationForm()
+    return render(request, 'app/register.html', {'form': form})
+
+
